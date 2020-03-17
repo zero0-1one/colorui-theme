c67cdcb381adaa4c6faca21ddc179f2097169d79
ygy <zero0_1one@163.com>
2020-03-17 23:52:42 +0800

    cutheme 命令支持 配置文件 使用相对路径

--- bin/cutheme.js
+++ bin/cutheme.js
@@ -2,6 +2,7 @@
 const package = require('../package.json')
 const { program } = require('commander');
 const cutheme = require('../lib/cutheme')
+const path = require('path')
 
 program
   .version(package.version)
@@ -10,8 +11,8 @@ program
 
 
 
-
-const cfg = require(program.config)
+let cfgPath = path.isAbsolute(program.config) ? program.config : path.join(process.cwd(), program.config)
+const cfg = require(cfgPath)
 
 cutheme.doAll(cfg).then((dest) => {
   console.log('导出完成!!   导出目录:', dest)
